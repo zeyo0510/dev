cce70d0c12701f574d0698df681d7176feb9d48f
Zeyo Li <zeyo0510@gmail.com>
2024-12-28 17:22:55 +0800

    1

---
+++ Shell+InstallFont/+install+font.ps1
@@ -0,0 +1,20 @@
+$files = Get-ChildItem -Path . -Filter "*.json"
+##################################################
+$files | ForEach-Object {
+  $file = $_.FullName
+  $data = Get-Content -Path $file -Raw
+  $json = $data | ConvertFrom-Json
+  $json | ForEach-Object {
+    $name = $_.Name
+    $path = $_.Path
+    ##################################################
+    Write-Host "Installing $name($path)"
+    ##################################################
+    Set-ItemProperty  -Path  "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" `
+                      -Name  $name                                                      `
+                      -Type  "String"                                                   `
+                      -Value $path
+  }
+}
+##################################################
+Write-Host "OK"
