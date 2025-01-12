3c1a46371f6dd50b1c8a4b11b82ba671d701717d
Zeyo Li <zeyo0510@gmail.com>
2025-01-13 00:57:07 +0800

    110

---
+++ Clone-BrowserWeb-cookie-editor/fjs129/emanager129.js
@@ -0,0 +1,5 @@
+// Cookie Editor for Google Chrome™ - Copyright (c) 2024 hotcleaner.com - All Rights Reserved.
+// Minified by Google Closure Compiler 20240317
+'use strict';
+try{const c=function(a){a.preventDefault();a.stopPropagation();a=new Date;let b=parseInt(a.getTimezoneOffset()/60,10);a=-4<b&&-1>b?0<a.toString().indexOf("riti")?"HN3WSHB7ZGTUG":"5VA4KQJSN6H6L":"5PBCC9KEQGGYL";window.open("https://www.paypal.com/donate/?hosted_button_id="+a)};window.addEventListener("contextmenu",function(a){a&&a.isTrusted&&(a.preventDefault(),a.stopPropagation())},!0);window.addEventListener("click",function(a){if(a&&1==a.isTrusted){let b=a.target;"button"==b.localName?c(a):"a"==
+b.localName&&b.href.startsWith("https://www.hotcleaner.com/support-click-and-clean-development")&&c(a)}},!1);setTimeout(()=>{document.title=" Cookie Manager"},1618)}catch(c){console.info(c)};
