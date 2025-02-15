4b7a987c3bbc70ee58b3a75d521c3bf322c10e00
Zeyo Li <zeyo0510@gmail.com>
2025-02-15 18:43:11 +0800

    181

--- App.EnvironmentVariableEditor/Main/MainForm.ExportData.cs
+++ App.EnvironmentVariableEditor/Main/MainForm.ExportData.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
 /************************************************/
 namespace App.EnvironmentVariableEditor.Main
 {
@@ -6,7 +10,34 @@ namespace App.EnvironmentVariableEditor.Main
   {
     public void ExportData()
     {
-      // TODO: do anything...
+      SaveFileDialog dialog = new SaveFileDialog();
+      {
+        dialog.Filter = "Tab separated values|*.tsv|All files|*.*";
+      }
+      /************************************************/
+      if (dialog.ShowDialog(this) != DialogResult.OK)
+      {
+        if (MessageBox.Show("User canceled!", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+        {
+          return;
+        }
+      }
+      /************************************************/
+      StringBuilder data = new StringBuilder();
+      /************************************************/
+      this.ListView1.Items
+    . Cast<ListViewItem>()
+    . ToList()
+    . ForEach((_) => {
+        string a = _.SubItems[0].Text;
+        string b = _.SubItems[1].Text;
+        /************************************************/
+        data.AppendLine(string.Format("{0}\t{1}", a, b));
+      });
+      /************************************************/
+      File.WriteAllText(dialog.FileName, data.ToString(), Encoding.UTF8);
+      /************************************************/
+      MessageBox.Show("Export successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
     }
   }
 }
