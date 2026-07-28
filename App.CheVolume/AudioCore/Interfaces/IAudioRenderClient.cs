0bdc679f8ba2f837a961737a7a713ec8906eff0e
JC Li <zeyo0510@gmail.com>
2026-07-29 01:23:46 +0800

    314

--- App.CheVolume/AudioCore/Interfaces/IAudioRenderClient.cs
+++ App.CheVolume/AudioCore/Interfaces/IAudioRenderClient.cs
@@ -1,14 +1,21 @@
-using System;
+// https://learn.microsoft.com/en-us/windows/win32/api/audioclient/nn-audioclient-iaudiorenderclient
+/************************************************/
 using System.Runtime.InteropServices;
-
+/************************************************/
 namespace AudioCore.Interfaces
 {
-	[Guid("F294ACFC-3146-4483-A7BF-ADDCA7C260E2")]
-	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-	internal interface IAudioRenderClient
-	{
-		int GetBuffer(int P_0, out IntPtr P_1);
-
-		int ReleaseBuffer(int P_0, AudioClientBufferFlags P_1);
-	}
+  [Guid("F294ACFC-3146-4483-A7BF-ADDCA7C260E2")]
+  [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+  internal interface IAudioRenderClient
+  {
+    int GetBuffer(
+      int _NUM_FRAMES_REQUESTED_,
+      out IntPtr _PP_DATA_
+    );
+    /************************************************/
+    int ReleaseBuffer(
+      int _NUM_FRAMES_WRITTEN_,
+      AudioClientBufferFlags _DW_FLAGS_
+    );
+  }
 }
